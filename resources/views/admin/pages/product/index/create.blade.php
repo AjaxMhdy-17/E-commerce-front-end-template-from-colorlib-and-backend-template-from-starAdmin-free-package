d0a6d90d269c0afefde32c50329013ff109fa2cc
MahedyHasan0017 <mhdyhasanpust017@gmail.com>
2025-03-19 17:35:58 +0600

    create product running

--- resources/views/admin/pages/product/index/create.blade.php
+++ resources/views/admin/pages/product/index/create.blade.php
@@ -104,21 +104,39 @@
                                 </div>
                             </div>
 
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label>Select Multiple</label>
-                                    <select name="size[]" class="size_select" style="width: 100%;" multiple="multiple">
-                                        <option value="AL">Alabama</option>
-                                        <option value="WY">Wyoming</option>
-                                        <option value="AM">America</option>
-                                        <option value="CA">Canada</option>
-                                        <option value="RU">Russia</option>
-                                    </select>
-                                    @error('size')
-                                    <div class="error_message" style="position: fixed;">{{ $message }}</div>
-                                    @enderror
+                            <div class="col-md-4">
+                                <div class="form--group">
+                                    <label for="size">Product Size</label>
+                                    <input type="text" name="size[]" class="form__control" id="size" placeholder="Enter product size">
                                 </div>
                             </div>
+
+                            <div class="col-md-4">
+                                <div class="form--group">
+                                    <label for="color">Product Color</label>
+                                    <input type="text" name="color[]" class="form__control" id="color" placeholder="Enter product color">
+                                </div>
+                            </div>
+
+
+                            <div class="col-md-4">
+                                <div class="form--group">
+                                    <label for="color">Product Selling Price</label>
+                                    <input type="text" name="selling_price" class="form__control" id="selling_price" placeholder="Enter product Selling Price">
+                                </div>
+                            </div>
+
+
+                            <div class="col-12">
+
+                                <div class="form-group">
+                                    <label for="color">Product Details</label>
+                                    <textarea id="summernote" name="content" class="form-control"></textarea>
+                                </div>
+
+                            </div>
+
+
                         </div>
 
                         <div class="button-container">
@@ -131,28 +149,58 @@
     </div>
 </div>
 
-
 @endsection
+
+
 
 
 @push('css-lib')
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<link href="https://cdn.jsdelivr.net/npm/tom-select@2.2.2/dist/css/tom-select.css" rel="stylesheet">
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
+
 @endpush
+
+
+
 
 @push('js-lib')
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/tom-select@2.2.2/dist/js/tom-select.complete.min.js"></script>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
+
 @endpush
 
 
 @push('style')
 <style>
     .form-group {
-        margin: 10px 0 15px 0 !important;
+        margin: 10px 0 5px 0 !important;
+        /* margin: 30px 0 0 0 !important; */
     }
-</style>
 
+    .form-group label {
+        margin: 0px;
+    }
 
-<style>
+    .tom__select {
+        border: 1px solid #dee2e6 !important;
+        padding: 22px !important;
+        margin-top: 20px;
+    }
+
+    .ts-control {
+        padding: 10px;
+    }
+
+    .form--group label {
+        font-size: 0.812rem;
+    }
+
     .select2-selection__choice__remove:hover {
         background: white !important;
         color: #000 !important;
@@ -199,7 +247,20 @@
         box-shadow: none !important;
         min-width: 20px;
     }
+
+    .note-toolbar .note-btn i{
+        font-size: 12px ;
+    }
+
+    .note-placeholder{
+        font-size: 13px ;
+    }
+
 </style>
+
+
+
+
 
 @endpush
 
@@ -209,15 +270,61 @@
     $(document).ready(function() {
         $('.category_select').select2();
     });
-</script>
 
-
-<script>
     $(document).ready(function() {
         $('.size_select').select2({
             allowClear: true
         });
     });
 </script>
+
+
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        new TomSelect("#size", {
+            plugins: ['remove_button'],
+            persist: false,
+            create: true,
+            delimiter: ",",
+        });
+
+
+        new TomSelect("#color", {
+            plugins: ['remove_button'],
+            persist: false,
+            create: true,
+            delimiter: ",",
+        });
+
+        new TomSelect("#selling_price", {
+            plugins: ['remove_button'],
+            persist: false,
+            create: true,
+            maxItems: 1,
+            delimiter: ",",
+        });
+
+
+    });
+</script>
+
+
+<script>
+    $(document).ready(function() {
+        $('#summernote').summernote({
+            height: 300,
+            toolbar: [
+                ['style', ['bold', 'italic', 'underline', 'clear']],
+                ['font', ['strikethrough', 'superscript', 'subscript']],
+                ['fontsize', ['fontsize']],
+                ['color', ['color']],
+                ['para', ['ul', 'ol', 'paragraph']],
+                ['height', ['height']]
+            ],
+            placeholder: 'Write your content here...',
+        });
+    });
+</script>
+
 
 @endpush
